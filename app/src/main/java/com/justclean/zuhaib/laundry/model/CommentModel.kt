1bb97730416d9a9928f5cfcb8d3d54fca8e3ace4
zuhaib <zuhahmed@dha.gov.ae>
2020-12-18 00:32:10 +0400

    whole project is complete now upload to github

--- app/src/main/java/com/justclean/zuhaib/laundry/model/CommentModel.kt
+++
@@ -1,7 +0,0 @@
-package com.justclean.zuhaib.laundry.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-class CommentModel(@PrimaryKey val id: Int,val title: String?, val body: String?){}
--- app/src/main/java/com/justclean/zuhaib/laundry/model/FavModel.kt
+++
@@ -1,8 +0,0 @@
-package com.justclean.zuhaib.laundry.model
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-
-@Entity
-class FavModel(@PrimaryKey val id: Int, val title: String?, val body: String?,var isSync:Boolean = false){}
